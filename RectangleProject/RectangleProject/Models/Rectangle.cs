7c1c4fc6ed48feefbbebfef309cd4ca41e02fc52
Zoé Sólyom <sz1solzoe@vasvari.org>
2022-02-25 12:25:04 +0100

    kéééész:D

---
+++ RectangleProject/RectangleProject/Models/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleProject.Models
+{
+    public class Rectangle
+    {
+        private double sideA, sideB;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+        public double SideA
+        {
+            get
+            {
+                return sideA;
+            }
+            set
+            {
+                sideA = value;
+            }
+        }
+        public double SideB
+        {
+            get
+            {
+                return sideB;
+            }
+            set
+            {
+                sideB = value;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return Math.Round((sideA * sideB), 2);
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return Math.Round((2 * (sideA + sideB)), 2);
+            }
+        }
+        public override string ToString()
+        {
+            return "A téglalap oldalai: " + sideA + ", " + sideB + "\n Kerülete: " + Area + "\n Területe: " + Perimeter;
+        }
+    }
+}
--- RectangleProject/RectangleProject/Program.cs
+++ RectangleProject/RectangleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RectangleProject.Models;
 
 namespace RectangleProject
 {
